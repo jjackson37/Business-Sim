d24cc15a4457eb4e14d621513e4cbba67e0bed01
jjackson37 <turbojake37@gmail.com>
2017-01-20 13:51:04 +0000

    Added Game class

---
+++ Business_Sim/Game.cs
@@ -0,0 +1,21 @@
+﻿namespace Business_Sim
+{
+    class Game
+    {
+        public enum difficulty
+        {
+            Unknown = 0,
+            vEasy = 1,
+            Easy = 2,
+            Normal = 3,
+            Hard = 4,
+            vHard = 5,
+            Extreme = 6
+        }
+
+        public Game(difficulty gameDifficulty)
+        {
+
+        }
+    }
+}
--- Business_Sim/Menu.cs
+++ Business_Sim/Menu.cs
@@ -8,16 +8,21 @@ namespace Business_Sim
             bool isRunning = true;
             while (isRunning)
             {
-                Console.Write("-BUSINESS SIM-\n1.I want to business\n2.Let's continue business\n" +
-                    "3.How do I business\n4.This business is too busy\n");
+                Console.Write("-BUSINESS SIM-\n1.New business\n2.Load business\n" +
+                    "3.Business help\n4.Quit\n");
                 char userInput = Console.ReadKey(true).KeyChar;
                 switch (userInput)
                 {
                     case '1':
+                        Console.Write("What difficulty?\n1-vEasy\n2-Easy\n3-Normal\n4-Hard\n5-vHard\n6-Extreme\n");
+                        Game.difficulty difficultyInput = (Game.difficulty)(Console.ReadKey(true).KeyChar);
+                        Game currentGame = new Game(difficultyInput);
                         break;
                     case '2':
+                        //Load save file function
                         break;
                     case '3':
+                        //Help menu function
                         break;
                     case '4':
                         Console.WriteLine("Goodbye");
