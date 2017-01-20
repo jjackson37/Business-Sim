473426406d32b487f4d1f1e929cd343dfbdcc0f5
jjackson37 <turbojake37@gmail.com>
2017-01-20 11:05:46 +0000

    Created v1.0 dev branch

--- Business_Sim/Program.cs
+++ Business_Sim/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Business_Sim
+﻿namespace Business_Sim
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
