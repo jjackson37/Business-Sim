2a131c1c2f73e67b7e90f91604a71fb0324cd010
jjackson37 <turbojake37@gmail.com>
2017-02-07 09:32:04 +0000

    Revert "Created v1.0 branch"
    This reverts commit ae0a2fe2f81fc7b6ea7dcc01d6e6099c3400e5ac.

--- Business_Sim/Program.cs
+++ Business_Sim/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace Business_Sim
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Sim
 {
     internal class Program
     {
